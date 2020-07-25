86ac9303246b41dc07f8b51ab7400c6350d37e7e
BoonCY <53253718+CYbooN@users.noreply.github.com>
2020-07-25 23:53:06 +0800

    Update newServer.js

--- newServer.js
+++ newServer.js
@@ -33,51 +33,13 @@ mongo.connect('mongodb://127.0.0.1/', function(err,db){
 	 	if(err){
 		throw err;
 	};
-	 	console.log("done");
+	 	console.log("Save record");
   	});
 		 
 	 }, 5000);
 
-
-
-
-  	 
-
-
-//db.close();
 	});
 
-
-
-	 
-
-	 
-
-	//setInterval (function(){
-
-		//let patiendId = num++;
-	//	let temperature = (Math.random()*(38.3-36.1)+36.1).toFixed(1);
-		//let date = new Date().toLocaleString();
-		//	console.log(patiendId);
-	//		console.log(temperature);
-		//	console.log(date);
-	//}, 10000);
-
-
-	// client.on('connection', function(socket){
-	// 	let patient=db.collection('patient');
-
-	// 	sendStatus = function(s){
-	// 		socket.emit('status',s);
-	// 	}
-
-
-		//socket.emit('output', res);
-		//setInterval (function(){
-		//let temperature = Math.floor(Math.random()*38.3+36.1);
-		//}, 10000)
-		//let temperature = 
-		//console.log(temperature);
 	});
-//});
+
 
