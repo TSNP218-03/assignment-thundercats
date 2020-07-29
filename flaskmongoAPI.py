56548654255fe0041a67cc1d96c95e2aaf41eda5
BoonCY <53253718+CYbooN@users.noreply.github.com>
2020-07-29 21:32:25 +0800

    Add files via upload

---
+++ flaskmongoAPI.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
+mongo = PyMongo(app)
+
+#run app
+@app.route("/api", methods=['GET'])
+def api_page():
+    temperature = mongo.db.patients
+    output = []
+    for data in temperature.find():
+        output.append({'temperatutre' : data['temperature']})
+    return jsonify({'result': output})
+
+if __name__ == "__main__":
+    app.run(debug=True)
