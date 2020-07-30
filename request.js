706292150661c874da272f9f5a6baa624b79354b
Chia Yen <53253718+CYbooN@users.noreply.github.com>
2020-07-30 23:35:02 +0800

    Delete request.js

--- request.js
+++
@@ -1,9 +0,0 @@
-const client = require('socket.io').listen(10086).sockets;
-const request = require('request');
-
-const url = 'http://127.0.0.1:5000/api';
-request(url, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body); 
-    }
-});
