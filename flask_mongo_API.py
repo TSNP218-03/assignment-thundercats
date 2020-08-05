d3cce3c54517791e41c6419060dcf9bd65e7e16f
Chia Yen <53253718+CYbooN@users.noreply.github.com>
2020-08-05 17:10:28 +0800

    Update flask_mongo_API.py
    add a new route to display the latest 10 results
    add real-time chart

--- flask_mongo_API.py
+++ flask_mongo_API.py
@@ -1,18 +1,40 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, url_for
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+CORS(app)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
 mongo = PyMongo(app)
 
-#run app
-@app.route("/Api", methods=['GET'])
-def Api_page():
+
+# run app
+@app.route("/")
+@app.route("/real_time_chart")
+def real_time_chart_page():
     temperature = mongo.db.patients
     output = []
     for data in temperature.find():
-        output.append({'temperatutre' : data['temperature']})
-    return jsonify({'result': output})
+        output.append({'temperature': data['temperature']})
+    return render_template('index.html', output=output)
+
+
+@app.route("/api_all_result", methods=['GET'])
+def api_all_result_page():
+    temperature = mongo.db.patients
+    output = []
+    for data in temperature.find():
+        output.append({'temperature': data['temperature'], 'date': data['date']})
+    return jsonify(output)
+
+
+@app.route("/api_latest_result", methods=['GET'])
+def api_latest_result_page():
+    temperature = mongo.db.patients
+    output = []
+    for data in temperature.find().sort('date', -1).limit(10):
+        output.append({'temperature': data['temperature'], 'date': data['date']})
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
